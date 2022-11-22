52ba8c7dfcd93747f0170e7c972c21d9dd9dd875
doondoong <doondoong@github.com>
2022-11-22 11:06:48 +0900

    로그인페이지 이동 추가

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,14 +3,14 @@ import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
 
 const user = {
-    name: 'Tom Cook',
+    name: '최기원',
     email: 'tom@example.com',
     imageUrl:
         'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 };
 const navigation = [
     { name: 'Dashboard', href: '#', current: true },
-    { name: 'Team', href: '#', current: false },
+    { name: 'GoLogin', href: '/', current: false },
     { name: 'Projects', href: '#', current: false },
     { name: 'Calendar', href: '#', current: false },
     { name: 'Reports', href: '#', current: false },
@@ -28,14 +28,6 @@ function classNames(...classes) {
 export default function Main() {
     return (
         <>
-            {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-gray-100">
-        <body class="h-full">
-        ```
-      */}
             <div className='min-h-full'>
                 <Disclosure as='nav' className='bg-gray-800'>
                     {({ open }) => (
