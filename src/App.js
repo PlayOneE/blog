c7b680b0a2624d7b197fcfcff31f9ac6b6fb383f
doondoong <doondoong@github.com>
2022-11-22 09:49:52 +0900

    tailwind css 프레임워크 적용

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <div className='m-5 bg-yellow-500 text-blue-500 font-bold'>tailwindcss 를 적용했습니다.</div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
